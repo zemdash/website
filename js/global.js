78637b9916ef9f8c9ed0cdc23bc9fa71699a4246
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2018-08-05 18:32:03 -0400

    update copy

--- js/global.js
+++ js/global.js
@@ -1,15 +1,3 @@
 $(function() {
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    var target = $(this.hash);
-    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-    if (target.length) {
-      $("html, body").animate(
-        {
-          scrollTop: target.offset().top - 70
-        },
-        1000
-      );
-      return false;
-    }
-  });
+
 });
